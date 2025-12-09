9b2fcf179c195425281963173a5849b16586240c
antoniomj <antoniomjr@users.noreply.github.com>
2025-12-09 12:23:15 -0300

    add unit teste

--- HackerNewsApi.Tests/StoriesControllerTests.cs
+++ HackerNewsApi.Tests/StoriesControllerTests.cs
@@ -1,6 +1,4 @@
 ﻿using System.Net;
-using System.Net.Http;
-using System.Linq;
 using System.Text.Json;
 using codingTest.Controllers;
 using codingTest.Models;
@@ -24,6 +22,18 @@ public class StoriesControllerTests
         var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
 
         var result = await controller.GetBestStories(0);
+
+        var badRequest = result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        badRequest.Value.Should().BeEquivalentTo(new { error = "Parameter 'n' must be greater than zero" });
+        _serviceMock.Verify(s => s.GetBestStoriesAsync(It.IsAny<int>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task GetBestStories_ReturnsBadRequest_WhenNIsNegative()
+    {
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        var result = await controller.GetBestStories(-5);
 
         var badRequest = result.Should().BeOfType<BadRequestObjectResult>().Subject;
         badRequest.Value.Should().BeEquivalentTo(new { error = "Parameter 'n' must be greater than zero" });
@@ -59,6 +69,125 @@ public class StoriesControllerTests
         var ok = result.Should().BeOfType<OkObjectResult>().Subject;
         ok.Value.Should().BeEquivalentTo(stories);
         _serviceMock.Verify(s => s.GetBestStoriesAsync(2), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBestStories_UsesDefaultValue_WhenNIsNotProvided()
+    {
+        var stories = new List<HackerNewStory>
+        {
+            new() { Title = "Story 1", Score = 10 }
+        };
+        _serviceMock.Setup(s => s.GetBestStoriesAsync(10)).ReturnsAsync(stories);
+
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        var result = await controller.GetBestStories();
+
+        var ok = result.Should().BeOfType<OkObjectResult>().Subject;
+        _serviceMock.Verify(s => s.GetBestStoriesAsync(10), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBestStories_ReturnsOk_WhenNIs500()
+    {
+        var stories = new List<HackerNewStory>();
+        _serviceMock.Setup(s => s.GetBestStoriesAsync(500)).ReturnsAsync(stories);
+
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        var result = await controller.GetBestStories(500);
+
+        result.Should().BeOfType<OkObjectResult>();
+        _serviceMock.Verify(s => s.GetBestStoriesAsync(500), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBestStories_ReturnsOk_WhenNIs1()
+    {
+        var stories = new List<HackerNewStory>
+        {
+            new() { Title = "Story 1", Score = 10 }
+        };
+        _serviceMock.Setup(s => s.GetBestStoriesAsync(1)).ReturnsAsync(stories);
+
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        var result = await controller.GetBestStories(1);
+
+        result.Should().BeOfType<OkObjectResult>();
+        _serviceMock.Verify(s => s.GetBestStoriesAsync(1), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBestStories_ReturnsInternalServerError_WhenServiceThrowsException()
+    {
+        _serviceMock.Setup(s => s.GetBestStoriesAsync(It.IsAny<int>()))
+            .ThrowsAsync(new Exception("Service error"));
+
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        var result = await controller.GetBestStories(10);
+
+        var statusCode = result.Should().BeOfType<ObjectResult>().Subject;
+        statusCode.StatusCode.Should().Be(500);
+        statusCode.Value.Should().BeEquivalentTo(new { error = "Error processing request" });
+    }
+
+    [Fact]
+    public async Task GetBestStories_LogsInformation_WhenRequestIsValid()
+    {
+        var stories = new List<HackerNewStory>();
+        _serviceMock.Setup(s => s.GetBestStoriesAsync(10)).ReturnsAsync(stories);
+
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        await controller.GetBestStories(10);
+
+        _loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains("Request to get 10 best stories")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBestStories_LogsError_WhenExceptionOccurs()
+    {
+        var exception = new Exception("Service error");
+        _serviceMock.Setup(s => s.GetBestStoriesAsync(It.IsAny<int>()))
+            .ThrowsAsync(exception);
+
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        await controller.GetBestStories(10);
+
+        _loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains("Error processing request")),
+                exception,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task GetBestStories_ReturnsEmptyList_WhenServiceReturnsEmpty()
+    {
+        var stories = new List<HackerNewStory>();
+        _serviceMock.Setup(s => s.GetBestStoriesAsync(10)).ReturnsAsync(stories);
+
+        var controller = new StoriesController(_serviceMock.Object, _loggerMock.Object);
+
+        var result = await controller.GetBestStories(10);
+
+        var ok = result.Should().BeOfType<OkObjectResult>().Subject;
+        var returnedStories = ok.Value.Should().BeAssignableTo<IEnumerable<HackerNewStory>>().Subject;
+        returnedStories.Should().BeEmpty();
     }
 }
 
@@ -162,6 +291,150 @@ public class HackerNewServiceTests
 
         bestStoriesCalls.Should().Be(1, "best story ids should be cached");
         itemCalls.Should().Be(2, "each story is cached and should be fetched once");
+    }
+
+    [Fact]
+    public async Task GetBestStoriesAsync_ReturnsEmptyList_WhenBestStoriesApiReturnsEmptyArray()
+    {
+        var handler = new RecordingHttpMessageHandler(request =>
+        {
+            var bestIds = JsonSerializer.Serialize(Array.Empty<int>());
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(bestIds)
+            };
+        });
+
+        var service = CreateService(handler);
+
+        var stories = (await service.GetBestStoriesAsync(10)).ToList();
+
+        stories.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GetBestStoriesAsync_ReturnsRequestedNumber_WhenMoreStoriesAvailable()
+    {
+        var handler = new RecordingHttpMessageHandler(request =>
+        {
+            if (request.RequestUri!.AbsolutePath.EndsWith("beststories.json"))
+            {
+                var bestIds = JsonSerializer.Serialize(new[] { 1, 2, 3, 4, 5 });
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(bestIds)
+                };
+            }
+
+            var id = request.RequestUri!.AbsolutePath.Split('/').Last().Replace(".json", "");
+            var itemPayload = JsonSerializer.Serialize(new HackerNewItem 
+            { 
+                Title = $"Story {id}", 
+                Url = $"http://{id}", 
+                By = "author", 
+                Time = int.Parse(id), 
+                Score = int.Parse(id) * 10, 
+                Descendants = 1 
+            });
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(itemPayload)
+            };
+        });
+
+        var service = CreateService(handler);
+
+        var stories = (await service.GetBestStoriesAsync(3)).ToList();
+
+        stories.Should().HaveCount(3);
+    }
+
+    [Fact]
+    public async Task GetBestStoriesAsync_HandlesMultipleFailedRequests()
+    {
+        var handler = new RecordingHttpMessageHandler(request =>
+        {
+            if (request.RequestUri!.AbsolutePath.EndsWith("beststories.json"))
+            {
+                var bestIds = JsonSerializer.Serialize(new[] { 1, 2, 3, 4, 5 });
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(bestIds)
+                };
+            }
+
+            if (request.RequestUri!.AbsolutePath.Contains("/1") || request.RequestUri!.AbsolutePath.Contains("/3"))
+            {
+                var id = request.RequestUri!.AbsolutePath.Split('/').Last().Replace(".json", "");
+                var itemPayload = JsonSerializer.Serialize(new HackerNewItem 
+                { 
+                    Title = $"Story {id}", 
+                    Url = $"http://{id}", 
+                    By = "author", 
+                    Time = int.Parse(id), 
+                    Score = int.Parse(id) * 10, 
+                    Descendants = 1 
+                });
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(itemPayload)
+                };
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+        });
+
+        var service = CreateService(handler);
+
+        var stories = (await service.GetBestStoriesAsync(5)).ToList();
+
+        stories.Should().HaveCount(2);
+        stories.Select(s => s.Title).Should().Contain(new[] { "Story 1", "Story 3" });
+    }
+
+    [Fact]
+    public async Task GetBestStoriesAsync_MapsAllFieldsCorrectly()
+    {
+        var handler = new RecordingHttpMessageHandler(request =>
+        {
+            if (request.RequestUri!.AbsolutePath.EndsWith("beststories.json"))
+            {
+                var bestIds = JsonSerializer.Serialize(new[] { 1 });
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(bestIds)
+                };
+            }
+
+            var itemPayload = JsonSerializer.Serialize(new HackerNewItem 
+            { 
+                Title = "Test Story", 
+                Url = "http://test.com", 
+                By = "testauthor", 
+                Time = 1234567890, 
+                Score = 100, 
+                Descendants = 50 
+            });
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(itemPayload)
+            };
+        });
+
+        var service = CreateService(handler);
+
+        var stories = (await service.GetBestStoriesAsync(1)).ToList();
+
+        stories.Should().HaveCount(1);
+        var story = stories.First();
+        story.Title.Should().Be("Test Story");
+        story.Uri.Should().Be("http://test.com");
+        story.PostedBy.Should().Be("testauthor");
+        story.Time.Should().Be(DateTimeOffset.FromUnixTimeSeconds(1234567890).DateTime);
+        story.Score.Should().Be(100);
+        story.CommentCount.Should().Be(50);
     }
 
     private static HackerNewService CreateService(HttpMessageHandler handler)
